9d025a09c74ba0cfcb9b48154f2a0100830f0083
HoaiNam1511 <90500710+HoaiNam1511@users.noreply.github.com>
2023-03-05 20:19:57 +0700

    change lazy

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import MainLayout from "./layout/MainLayout/MainLayout";
 function App() {
     return (
         <Router>
-            <Suspense fallback={<div>Loading ...</div>}>
-                <Routes>
-                    {publicRouter.map((route, index) => {
-                        let Layout = MainLayout;
-                        if (route.layout === null) {
-                            Layout = Fragment;
-                        } else if (route.layout) {
-                            Layout = route.layout;
-                        }
-                        const Page = route.component;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page></Page>
-                                    </Layout>
-                                }
-                            ></Route>
-                        );
-                    })}
-                </Routes>
-            </Suspense>
+            {/* <Suspense fallback={<div>Loading ...</div>}> */}
+            <Routes>
+                {publicRouter.map((route, index) => {
+                    let Layout = MainLayout;
+                    if (route.layout === null) {
+                        Layout = Fragment;
+                    } else if (route.layout) {
+                        Layout = route.layout;
+                    }
+                    const Page = route.component;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page></Page>
+                                </Layout>
+                            }
+                        ></Route>
+                    );
+                })}
+            </Routes>
+            {/* </Suspense> */}
         </Router>
     );
 }
