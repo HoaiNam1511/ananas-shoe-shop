8603cfe8ddc95169243274d0162fab2a26321d5e
HoaiNam1511 <90500710+HoaiNam1511@users.noreply.github.com>
2023-03-05 15:32:26 +0700

    optimize lazy component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRouter } from "./router";
 import MainLayout from "./layout/MainLayout/MainLayout";
@@ -6,28 +6,30 @@ import MainLayout from "./layout/MainLayout/MainLayout";
 function App() {
     return (
         <Router>
-            <Routes>
-                {publicRouter.map((route, index) => {
-                    let Layout = MainLayout;
-                    if (route.layout === null) {
-                        Layout = Fragment;
-                    } else if (route.layout) {
-                        Layout = route.layout;
-                    }
-                    const Page = route.component;
-                    return (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={
-                                <Layout>
-                                    <Page></Page>
-                                </Layout>
-                            }
-                        ></Route>
-                    );
-                })}
-            </Routes>
+            <Suspense fallback={<div>Loading ...</div>}>
+                <Routes>
+                    {publicRouter.map((route, index) => {
+                        let Layout = MainLayout;
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        } else if (route.layout) {
+                            Layout = route.layout;
+                        }
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page></Page>
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </Suspense>
         </Router>
     );
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,26 @@
+import { lazy } from "react";
 import config from "../config";
-import Cart from "../page/Cart/Cart";
-import Home from "../page/Home/Home";
-import Product from "../page/Product/Product";
-import ProductDetail from "../page/ProductDetail/ProductDetail";
-import SearchOrder from "../page/SearchOrder/SearchOrder";
-import Store from "../page/Store/Store";
-import WishList from "../page/WishList/WishList";
-import MainLayout from "../layout/MainLayout/MainLayout";
-import Order from "../page/Order/Order";
-import Search from "../page/Search/Search";
+// import Cart from "../page/Cart/Cart";
+// import Home from "../page/Home/Home";
+// import Product from "../page/Product/Product";
+// import ProductDetail from "../page/ProductDetail/ProductDetail";
+// import SearchOrder from "../page/SearchOrder/SearchOrder";
+// import Store from "../page/Store/Store";
+// import WishList from "../page/WishList/WishList";
+// import MainLayout from "../layout/MainLayout/MainLayout";
+// import Order from "../page/Order/Order";
+// import Search from "../page/Search/Search";
+
+const Cart = lazy(() => import("../page/Cart/Cart"));
+const Home = lazy(() => import("../page/Home/Home"));
+const Product = lazy(() => import("../page/Product/Product"));
+const ProductDetail = lazy(() => import("../page/ProductDetail/ProductDetail"));
+const SearchOrder = lazy(() => import("../page/SearchOrder/SearchOrder"));
+const Store = lazy(() => import("../page/Store/Store"));
+const WishList = lazy(() => import("../page/WishList/WishList"));
+const MainLayout = lazy(() => import("../layout/MainLayout/MainLayout"));
+const Order = lazy(() => import("../page/Order/Order"));
+const Search = lazy(() => import("../page/Search/Search"));
 
 export const publicRouter = [
     {
